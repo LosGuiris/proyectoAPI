07a06f5a2768ecdad77d86daa4b952ffc6d74ab2
muflonex <muflonex@gmail.com>
2018-04-24 21:58:46 +0200

    It works. We had to reset api link after performing ajax

--- routes/search.js
+++ routes/search.js
@@ -13,24 +13,29 @@ router.get("/", (req, res, next) => {
 
 /* POST query */
 router.post("/", (req, res, next) => {
-  console.log(req.body);
+  // console.log(req.body);
   // Adding q parameter
-  if (req.body.name != undefined) {
+  if (req.body.name != "") {
     let name = req.body.name;
-    if (name != "") {
-      api += `&q=${name}`;
-    }
+    // console.log(`name: ${name}`)
+    api += `&q=${name}`;
   }
   //  Adding all diet parameters
-  api += multiParams("diet", req.body.diet);
+  if (typeof req.body.diet !== undefined) {
+    // console.log(req.body.diet)
+    api += multiParams("diet", req.body.diet);
+  }
   //  Adding all health parameters
-  api += multiParams("health", req.body.health);
+  if (typeof req.body.health !== undefined) {
+    // console.log(req.body.health)
+    api += multiParams("health", req.body.health);
+  }
   //  Adding calories MIN/MAX parameters
   api += minmaxParams("&calories=", req.body.cal_min, req.body.cal_max);
   // Adding preparation time MIN/MAX parameters
   api += minmaxParams("&time=", req.body.t_min, req.body.t_max);
 
-  console.log(`Request URL: ${api}`);
+   console.log(`Request URL: ${api}`);
   res.redirect("/search/:results");
 });
 
@@ -41,24 +46,27 @@ router.get("/:results", (req, res, next) => {
     .then(recipe => {
       // Pushing all results
       let recs = recipe.data.hits;
+      console.log(recs)
+      // console.log(recs)
+      api = `https://api.edamam.com/search?app_id=${app_id}&app_key=${app_key}`;
       res.render("search/index", { recs });
     })
     .catch(error => console.log(error));
 });
 //  Refactored function for both multiple choice parameters
 const multiParams = (p_name, p) => {
-  if (p === undefined) {
-    return (p = "");
-  } else {
-    if (typeof p === "string") {
-      return `&${p_name}=${p}`;
-    } else if (typeof p === "object") {
-      let acc = ""
-      for (let i = 0; i < p.length; i++) {
-        acc += `&${p_name}=${p[i]}`;
-      }
-      return acc;
+  if (!p){
+    return p = ""
+  }
+  if (typeof p === "string") {
+    return `&${p_name}=${p}`;
+  }
+  if (typeof p === "object") {
+    let acc="";
+    for (let i = 0; i < p.length; i++) {
+      acc += `&${p_name}=${p[i]}`;
     }
+    return acc;
   }
 };
 //  Refactored function for both parameters IF chains
